dfb7e506e8dd7f3d9ea85e2789ed65248603683d
lorenzo <lor.abbadessa@gmail.com>
2023-12-05 17:14:29 +0100

    costruisce milestone 1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+
+import { store } from './components/store.js'
+import axios from 'axios';
+
+const options = {
+  method: 'GET',
+  url: 'https://api.themoviedb.org/3/authentication',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2N2I0ZDM1ZmRiZThjNzQ0ZjlhZjhhYWE0Y2M0MDMyZSIsInN1YiI6IjY1NmRkY2JlM2RjMzEzMDBlMWVlOTdiNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.k1By06hgxySVaizPmjXAkNlNBLe6Fctpai_fwoc-avA'
+  }
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
+
+</script>
+
+
+
+<template>
+  <input type="text" placeholder="cerca">
+  <button>VAI!</button>
+</template>
+
+
+
+<style scoped></style>
